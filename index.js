dce94206ad90fe343d65b25834f5e306609aaf26
jinjor <jinjorweb@gmail.com>
2018-11-04 15:36:03 +0900

    separate cli and index

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const cp = require("child_process");
 const fs = require("fs-extra");
 const path = require("path");
-const argv = require("argv");
 
 const cleanup = [];
 process.on("exit", function(code, signal) {
@@ -12,66 +11,38 @@ process.on("exit", function(code, signal) {
   }
 });
 
-argv.info("Usage: gitscaf user/project src target [options]");
-argv.option([
-  {
-    name: "server",
-    type: "string",
-    description: "Use custom server (default: github.com)"
-  },
-  {
-    name: "protocol",
-    type: "string",
-    description: "Use one of [ https, git ] (default: https)"
-  },
-  {
-    name: "branch",
-    short: "b",
-    type: "string",
-    description: "Fetch a particular branch (default: master)"
-  },
-  {
-    name: "force",
-    short: "f",
-    type: "boolean",
-    description: "Override existing target (default: false)"
+module.exports = function gitscaf(userAndProject, src, target, options) {
+  options = options || {};
+  const tmpDir = "/tmp/gitscaf";
+  const server = options.server || "github.com";
+  const protocol = options.protocol || "https";
+  const branch = options.branch || "master";
+  const force = options.force || false;
+
+  const templatePath = path.join(tmpDir, src);
+  const gitUrl =
+    protocol === "git"
+      ? `git@${server}:${userAndProject}.git`
+      : protocol === "https"
+        ? `https://${server}/${userAndProject}.git`
+        : (() => {
+            throw new Error(`Unsupported protocol: ${protocol}`);
+          })();
+  if (!force && fs.existsSync(target)) {
+    throw new Error(`Target "${target}" already exists`);
   }
-]);
-const { targets, options } = argv.run();
-const [userAndProject, src, target] = targets;
-if (!target) {
-  argv.help();
-  process.exit(1);
-}
-const tmpDir = "/tmp/gitscaf";
-const server = options.server || "github.com";
-const protocol = options.protocol || "https";
-const branch = options.branch || "master";
-const force = options.force || false;
-const templatePath = path.join(tmpDir, src);
+  cleanup.push(() => fs.removeSync(target));
 
-const gitUrl =
-  protocol === "git"
-    ? `git@${server}:${userAndProject}.git`
-    : protocol === "https"
-      ? `https://${server}/${userAndProject}.git`
-      : (console.error(`Unsupported protocol: ${protocol}`), process.exit(1));
-
-if (!force && fs.existsSync(target)) {
-  console.error(`Target "${target}" already exists`);
-  process.exit(1);
-}
-cleanup.push(() => fs.removeSync(target));
-
-fs.removeSync(tmpDir);
-if (userAndProject.startsWith("/") || userAndProject.startsWith(".")) {
-  // debug mode
-  const projectPath = path.resolve(userAndProject);
-  fs.copySync(projectPath, tmpDir);
-} else {
-  cleanup.push(() => fs.removeSync(tmpDir));
-  cp.execSync(`git clone ${gitUrl} ${tmpDir} -b ${branch} --depth 1 --quiet`);
-  fs.removeSync(path.join(templatePath, ".git"));
-}
-fs.copySync(templatePath, target);
-fs.removeSync(tmpDir);
+  fs.removeSync(tmpDir);
+  if (userAndProject.startsWith("/") || userAndProject.startsWith(".")) {
+    // debug mode
+    const projectPath = path.resolve(userAndProject);
+    fs.copySync(projectPath, tmpDir);
+  } else {
+    cleanup.push(() => fs.removeSync(tmpDir));
+    cp.execSync(`git clone ${gitUrl} ${tmpDir} -b ${branch} --depth 1 --quiet`);
+    fs.removeSync(path.join(templatePath, ".git"));
+  }
+  fs.copySync(templatePath, target);
+  fs.removeSync(tmpDir);
+};
--- test.sh
+++ test.sh
@@ -4,25 +4,25 @@ rm -rf tmp
 mkdir tmp
 
 # too few arguments
-node index . example > /dev/null 2>&1
+node cli . example > /dev/null 2>&1
 [[ $? = 0 ]] && exit 1
 # unknown options
-node index . example tmp/p --unknown > /dev/null 2>&1
+node cli . example tmp/p --unknown > /dev/null 2>&1
 [[ $? = 0 ]] && exit 1
 # unknown protocol
-node index . example tmp/p --protocol=unknown > /dev/null 2>&1
+node cli . example tmp/p --protocol=unknown > /dev/null 2>&1
 [[ $? = 0 ]] && exit 1
 # template dir not found
-node index . not_exist tmp/p > /dev/null 2>&1
+node cli . not_exist tmp/p > /dev/null 2>&1
 [[ $? = 0 ]] && exit 1
 # existing dir
-node index . example tmp > /dev/null 2>&1
+node cli . example tmp > /dev/null 2>&1
 [[ $? = 0 ]] && exit 1
 
 set -e
 
-node index . example tmp/p1
-node index . example/public tmp/p2
-node index ./example . tmp/deeply/nested/p3
-node index . example/public/index.html tmp/index.html
-node index -f . example/public/index.html tmp/index.html
+node cli . example tmp/p1
+node cli . example/public tmp/p2
+node cli ./example . tmp/deeply/nested/p3
+node cli . example/public/index.html tmp/index.html
+node cli -f . example/public/index.html tmp/index.html
